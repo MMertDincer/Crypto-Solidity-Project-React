d1cd5bccc3b4c082d8f292ba215fe7c3a5cc48a1
MMertDincer <mert.dincer05@gmail.com>
2022-05-12 11:58:53 +0300

    Transaction wallet to wallet ++

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,1 +1,6 @@
 //0x48b7A7E24C80fcb40341A6810e93c125CC2054A7
+
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x48b7A7E24C80fcb40341A6810e93c125CC2054A7';
